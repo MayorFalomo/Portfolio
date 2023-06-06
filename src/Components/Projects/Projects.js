e81ff54b8617576384e24a57b11658c8dede3d5f
MayorFalomo <mayorfalomo@gmail.com>
2023-06-06 17:58:34 +0100

    added tweeks to projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -28,17 +28,17 @@ const Projects = () => {
 
       <div className="projectWrapper" >
 
-        <div className="subProjectLeft" data-aos="fade-up" style={{ backgroundImage: "url(./blogaroo.png)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+        <div className="subProjectLeft" data-aos="fade-up" style={{ backgroundImage: "url(./tweeks.png)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
           <div className='overLayLeft' ></div>
           <div className="parallaxText">
-            <h1>Blogaroo</h1>
-            <p>A creative Blog Website i built using the MERN Stack, it has all CRUD functionalities and added features of likes and comments on blog post, editing your profile and making a blog post.</p>
-            <span>MongoDb, Express,Typescript, React, Node Js and Styled-Components </span>
+            <h1>Tweeks - A Twitter Clone </h1>
+            <p>Tweeks is a Full-Stack social web app i built using the MENN Stack and firebase, users can real-time message, get notifications, retweet, like, quote and bookmark tweets, post tweets, like and reply comments  it has all CRUD functionalities</p>
+            <span>MongoDb, Express,Typescript, Next JS, Node Js, Firebase, PWA and Styled-Components </span>
             <div className="flexLinks">
-              <a id="black" target="_blank" href="https://blogaroo.netlify.app/register"  rel="noreferrer">
+              <a id="black" target="_blank" href="https://tweeks.netlify.app/register"  rel="noreferrer">
                 {<MdLogout className="netlifyLink" />} Demo{" "}
               </a>
-              <a id="black" target="_blank" href="https://github.com/MayorFalomo/Blogaroo-FrontEnd"  rel="noreferrer">
+              <a id="black" target="_blank" href="https://github.com/MayorFalomo/Twitter-clone"  rel="noreferrer">
                 {<IoLogoGithub className="githubLink" />} Github{" "}
               </a>
             </div>
@@ -46,54 +46,75 @@ const Projects = () => {
         </div>
         
         <div className="subProjectRightCon" >
-          <div className="subProjectRight" data-aos="fade-up" style={{ backgroundImage: "url(./cinemaflix.png)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+          <div className="subProjectRight" data-aos="fade-up" style={{ backgroundImage: "url(./blogaroo.png)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
             <div className='overLayRight' ></div>
             <div id="parallax">
-            <h1> CinemaFlix</h1>
-              <p>A movie hub project using the TMDB API, You can add movies to your watchlist, search for any movie, watch trailers for the series and browse through movies</p>
-              <span>React, CSS, API </span>
+            <h1> Blogaroo</h1>
+              <p>A creative Blog Website i built using the MERN Stack, it has all CRUD functionalities and added features of likes and comments on blog post, editing your profile and making a blog post.</p>
+              <span>MongoDb, Express,Typescript, React, Node Js and Styled-Components </span>
             <div className="flexLink">
               <a
                 className="link"
                 target="_blank"
-              href="https://cinema-flix.netlify.app/"
+              href="https://blogaroo.netlify.app/register"
               rel="noreferrer"
               >
                 {<MdLogout className="netlifyLink" />} Demo{" "}
               </a>
-              <a target="_blank" href="https://github.com/MayorFalomo/cinemaFlix" rel="noreferrer">
+              <a target="_blank" href="https://github.com/MayorFalomo/Blogaroo-FrontEnd" rel="noreferrer">
                 {<IoLogoGithub className="githubLink" />} Github{" "}
               </a>
           </div>
             </div>
             </div>
           </div>
+      
 
-        <div className="subProjectLeft"  style={{backgroundImage: "url(./medleyGames.jpg)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundColor: 'black'}} data-aos="fade-up">
+        <div className="subProjectLeft"  style={{backgroundImage: "url(./cinemaflix.png)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundColor: 'black'}} data-aos="fade-up">
             <div className='overLayLeft' ></div>
           <div className="parallaxText">
-            <h1>Medley Games </h1>
-            <p>A mock Game website page i built using React and CSS, It uses react-router to route to different pages </p>
-            <span>React, CSS </span>
+            <h1> CinemaFlix </h1>
+            <p>A movie hub project using the TMDB API, You can add movies to your watchlist, search for any movie, watch trailers for the series and browse through movies </p>
+            <span>React, CSS, API </span>
             <div className="flexLinks">
-              <a target="_blank" href="https://medley-games.netlify.app/" rel="noreferrer">
+              <a target="_blank" href="https://cinema-flix.netlify.app/" rel="noreferrer">
                 {<MdLogout className="netlifyLink" />} Demo
               </a>
-              <a target="_blank" href="https://github.com/MayorFalomo/Medley-Games" rel="noreferrer">
+              <a target="_blank" href="https://github.com/MayorFalomo/cinemaFlix" rel="noreferrer">
                 {<IoLogoGithub className="githubLink" />} Github
               </a>
             </div>
           </div>
-          </div>
+        </div>
+        
        
                 <div className="subProjectRightCon" >
-        <div className="subProjectRight"  data-aos="fade-up" style={{backgroundImage: "url(./urban.jpg)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
+        <div className="subProjectRight"  data-aos="fade-up" style={{backgroundImage: "url(./medleyGames.jpg)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
               <div className='overLayRight' ></div>
             <div id="parallax">
-            <h1>Nft Dashboard </h1>
-              <p>A Single Page Nft Dashboard, It uses local storage to save your dark mode or light mode color</p>
+            <h1>Medley Games </h1>
+              <p>A mock Game website page i built using React and CSS, It uses react-router to route to different pages</p>
               <span> React, CSS</span>
             <div className="flexLink">
+              <a target="_blank" href="https://medley-games.netlify.app/" rel="noreferrer">
+                {<MdLogout className="netlifyLink" />} Demo{" "}
+              </a>
+              <a target="_blank" href="https://github.com/MayorFalomo/Medley-Games" rel="noreferrer">
+                {<IoLogoGithub className="githubLink" />} Github{" "}
+              </a>
+            </div>
+          </div>
+          </div>
+        </div>
+        
+       
+        <div className="subProjectLeft" style={{backgroundImage: "url(./urban.jpg)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}  data-aos="fade-up">
+          <div className='overLayLeft' ></div>
+          <div className="parallaxText">
+            <h1>Nft Dashboard</h1>
+            <p>A Single Page Nft Dashboard, It uses local storage to save your dark mode or light mode color </p>
+            <span>React, CSS </span>
+            <div className="flexLinks">
               <a target="_blank" href="https://urban-nft-dashboard.netlify.app/" rel="noreferrer">
                 {<MdLogout className="netlifyLink" />} Demo{" "}
               </a>
@@ -102,16 +123,17 @@ const Projects = () => {
               </a>
             </div>
           </div>
-          </div>
-          </div>
+        </div>
+        
        
-        <div className="subProjectLeft" style={{backgroundImage: "url(./travelBuddy.jpg)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}  data-aos="fade-up">
-          <div className='overLayLeft' ></div>
-          <div className="parallaxText">
-            <h1>Travel Buddy</h1>
-            <p>A mock Travel Agency Site i built using React also uses react router for routing between pages this was my second ever project using React </p>
-            <span>React, CSS </span>
-            <div className="flexLinks">
+                <div className="subProjectRightCon" >
+        <div className="subProjectRight" style={{backgroundImage: "url(./travelBuddy.jpg)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}} data-aos="fade-up">
+            <div className='overLayRight' ></div>
+            <div id="parallax">
+            <h1>Travel Buddy </h1>
+              <p>A mock Travel Agency Site i built using React also uses react router for routing between pages this was my second ever project using React  </p>
+              <span>React, CSS</span>
+            <div className="flexLink">
               <a target="_blank" href="https://travelbuddy-agency.netlify.app/" rel="noreferrer">
                 {<MdLogout className="netlifyLink" />} Demo{" "}
               </a>
@@ -119,75 +141,58 @@ const Projects = () => {
                 {<IoLogoGithub className="githubLink" />} Github{" "}
               </a>
             </div>
-          </div>
-          </div>
-       
-                <div className="subProjectRightCon" >
-        <div className="subProjectRight" style={{backgroundImage: "url(./orpheus.png)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}} data-aos="fade-up">
-            <div className='overLayRight' ></div>
-            <div id="parallax">
-            <h1>Orpheus </h1>
-              <p> A single Landing Page using React </p>
-              <span>React, SASS</span>
-            <div className="flexLink">
-              <a target="_blank" href="https://orpheus-nft.netlify.app/" rel="noreferrer">
-                {<MdLogout className="netlifyLink" />} Demo{" "}
-              </a>
-              <a target="_blank" href="https://github.com/MayorFalomo/Orpheus-landing-page"               rel="noreferrer">
-                {<IoLogoGithub className="githubLink" />} Github{" "}
-              </a>
-            </div>
             </div>
           </div>
           </div>
+  
       
-        <div className="subProjectLeft" style={{backgroundImage: "url(./google.png)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}  data-aos="fade-up">
+        <div className="subProjectLeft" style={{backgroundImage: "url(./orpheus.png)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}  data-aos="fade-up">
           <div className='overLayLeft' ></div>
           <div className="parallaxText">
-            <h1>Google Clone</h1>
-            <p>Google clone - A Search Engine i built using the programming search engine API and firebase for authentication, it works just like google   </p>
-            <span>HTML, React, CSS, API, Firebase</span>
+            <h1>Orpheus</h1>
+            <p>A single Landing Page using React  </p>
+            <span>React, SASS</span>
             <div className="flexLinks">
-              <a id="black" target="_blank" href="https://googlebase.netlify.app/" rel="noreferrer">
+              <a id="black" target="_blank" href="https://orpheus-nft.netlify.app/" rel="noreferrer">
                 {<MdLogout className="netlifyLink" />} Demo{" "}
               </a>
-              <a id="black" target="_blank" href="https://github.com/MayorFalomo/GoogleBase" rel="noreferrer">
+              <a id="black" target="_blank" href="https://github.com/MayorFalomo/Orpheus-landing-page" rel="noreferrer">
                 {<IoLogoGithub className="githubLink" />} Github{" "}
               </a>
             </div>
             </div>
-          </div>
+        </div>
        
                 <div className="subProjectRightCon" >
-        <div className="subProjectRight" style={{backgroundImage: "url(./swishUpdated.png)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}} data-aos="fade-up">
+        <div className="subProjectRight" style={{backgroundImage: "url(./google.png)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}} data-aos="fade-up">
             <div className='overLayRight' ></div>
             <div id="parallax">
-            <h1>Swish </h1>
-            <p> Weather app i built using the openweather API and restcountry API, I also added a converter so users can get their temperature in whatever unit they prefer and i used a rotating 3d image of Earth  <a target="_blank" href="https://github.com/temidayo-falomo" rel="noreferrer">  @Temidayo</a> also contributed to the UI </p>
-            <span>Typescript, React, Styled-components and API </span>
+            <h1>Google Clone </h1>
+            <p> Google clone - A Search Engine i built using the programming search engine API and firebase for authentication, it works just like google   </p>
+            <span>HTML, React, CSS, API, Firebase </span>
               <div className="flexLink">
-              <a target="_blank" href="https://swish-weather-app.netlify.app/" rel="noreferrer">
+              <a target="_blank" href="https://googlebase.netlify.app/" rel="noreferrer">
                 {<MdLogout className="netlifyLink" />} Demo{" "}
               </a>
-              <a target="_blank" href="https://github.com/MayorFalomo/Swish-Weather-App" rel="noreferrer">
+              <a target="_blank" href="https://github.com/MayorFalomo/GoogleBase" rel="noreferrer">
                 {<IoLogoGithub className="githubLink" />} Github{" "}
               </a>
             </div>
             </div>
           </div>
-          </div>
+        </div>
       
-        <div className="subProjectLeft" style={{backgroundImage: "url(./quiz.png)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}  data-aos="fade-up">
+        <div className="subProjectLeft" style={{backgroundImage: "url(./swishUpdated.png)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}  data-aos="fade-up">
                     <div className='overLayLeft' ></div>
           <div className="parallaxText">
-            <h1>Quiz App</h1>
-            <p>A Trivial App i built using Next Js, The inspiration for this was to model something after my University CBT app but using trivial questions instead </p>
+            <h1>Swish</h1>
+            <p>Weather app i built using the openweather API and restcountry API, I also added a converter so users can get their temperature in whatever unit they prefer and i used a rotating 3d image of Earth  <a target="_blank" href="https://github.com/temidayo-falomo" rel="noreferrer">  @Temidayo</a> also contributed to the UI </p>
             <span>Next JS, CSS </span>
             <div className="flexLinks">
-              <a id="black" target="_blank" href="https://trivial-quiz-app.netlify.app/" rel="noreferrer">
+              <a id="black" target="_blank" href="https://swish-weather-app.netlify.app/" rel="noreferrer">
                 {<MdLogout className="netlifyLink" />} Demo{" "}
               </a>
-              <a id="black" target="_blank" href="https://github.com/MayorFalomo/Quiz-App" rel="noreferrer">
+              <a id="black" target="_blank" href="https://github.com/MayorFalomo/Swish-Weather-App" rel="noreferrer">
                 {<IoLogoGithub className="githubLink" />} Github{" "}
               </a>
             </div>
@@ -195,17 +200,17 @@ const Projects = () => {
             </div>
        
                 <div className="subProjectRightCon" >
-        <div className="subProjectRight" style={{backgroundImage: "url(./StoreBay.png)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}  data-aos="fade-up">
+        <div className="subProjectRight" style={{backgroundImage: "url(./quiz.png)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}  data-aos="fade-up">
           <div className='overLayRight' ></div>
             <div id="parallax">
-            <h1>StoreBay </h1>
-              <p> An E-commerce site - My first Vue Js project, styled using Tailwind, the challenge here was using two new technologies for the first time and building something with it, the fashion products come from escuelajs Api. </p>
-              <span>Vue JS, Tailwind, API </span>
+            <h1>Quiz App </h1>
+              <p>A Trivial App i built using Next Js, The inspiration for this was to model something after my University CBT app but using trivial questions instead </p>
+              <span>Next JS, CSS </span>
             <div className="flexLink">
-              <a target="_blank" href="https://store-bay.netlify.app/" rel="noreferrer">
+              <a target="_blank" href="https://trivial-quiz-app.netlify.app/" rel="noreferrer">
                 {<MdLogout className="netlifyLink" />} Demo{" "}
               </a>
-              <a target="_blank" href="https://github.com/MayorFalomo/StoreBay" rel="noreferrer">
+              <a target="_blank" href="https://github.com/MayorFalomo/Quiz-App" rel="noreferrer">
                 {<IoLogoGithub className="githubLink" />} Github{" "}
               </a>
             </div>
@@ -213,7 +218,22 @@ const Projects = () => {
           </div>
           </div>
         
-        
+         <div className="subProjectLeft" style={{backgroundImage: "url(./StoreBay.png)", backgroundPosition: "center", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}  data-aos="fade-up">
+                    <div className='overLayLeft' ></div>
+          <div className="parallaxText">
+            <h1>StoreBay</h1>
+            <p> An E-commerce site - My first Vue Js project, styled using Tailwind, the challenge here was using two new technologies for the first time and building something with it, the fashion products come from escuelajs Api. </p>
+            <span>Next JS, CSS </span>
+            <div className="flexLinks">
+              <a id="black" target="_blank" href="https://store-bay.netlify.app/" rel="noreferrer">
+                {<MdLogout className="netlifyLink" />} Demo{" "}
+              </a>
+              <a id="black" target="_blank" href="https://github.com/MayorFalomo/StoreBay" rel="noreferrer">
+                {<IoLogoGithub className="githubLink" />} Github{" "}
+              </a>
+            </div>
+            </div>
+            </div>
         </div>
         </div>
   );
