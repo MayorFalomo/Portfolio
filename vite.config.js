247dbc9ac47c850bce1d2e63e9287e7bac03e548
mayor falomo <mayorfalomo@gmail.com>
2024-02-14 11:44:24 +0100

    changed dir to build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
   plugins: [react(), svgr()],
   build: {
-    outDir: "dist",
+    outDir: "build",
+    // outDir: "dist",
   },
 });
